b76d054deb69ee707921e262bb11508694dc844f
Jeremy Myers <echelonical@gmail.com>
2022-12-02 23:00:03 -0600

    updates to ServiceType model. auth.py added

--- mcpressureapi/models/service_type.py
+++ mcpressureapi/models/service_type.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class ServiceType(models.Model):
-    label = models.CharField()
-    description = models.CharField()
+    label = models.CharField(max_length=155)
+    description = models.CharField(max_length=200)
     location = models.ForeignKey("Location", on_delete=models.CASCADE)
     price = models.FloatField()
-    name =  models.CharField()
+    name =  models.CharField(max_length=75)
