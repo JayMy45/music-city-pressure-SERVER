df57c7f85329784fd7dd8d19fe0b36f632db8f67
Jeremy Myers <echelonical@gmail.com>
2022-12-03 18:38:29 -0600

    created fixtures for MVP models

--- seed_database.sh
+++ seed_database.sh
@@ -5,3 +5,14 @@ rm -rf ./mcpressureapi/migrations
 python3 manage.py migrate
 python3 manage.py makemigrations mcpressureapi
 python3 manage.py migrate mcpressureapi
+python3 manage.py loaddata user
+python3 manage.py loaddata tokens
+python3 manage.py loaddata customer
+python3 manage.py loaddata employee
+python3 manage.py loaddata location
+python3 manage.py loaddata progress
+python3 manage.py loaddata equipment
+python3 manage.py loaddata service_type
+python3 manage.py loaddata service_call
+python3 manage.py loaddata reviews
+python3 manage.py loaddata service_type_equipment
