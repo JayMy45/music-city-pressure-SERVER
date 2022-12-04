6f7b95424ec1cd01be8754b565b82856eecfb299
Jeremy Myers <echelonical@gmail.com>
2022-12-04 17:49:16 -0600

    created, imported and routed ServiceCallView and add functions to list All/One Service call

--- musiccitypressure/urls.py
+++ musiccitypressure/urls.py
@@ -16,10 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.conf.urls import include
 from django.urls import path
-from mcpressureapi.views import register_user, login_user
+from rest_framework import routers
+from mcpressureapi.views import register_user, login_user, ServiceCallView
+
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'calls', ServiceCallView, 'call')
 
 urlpatterns = [
     path('register', register_user),
     path('login', login_user),
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
