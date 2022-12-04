b2e2fb477649e1633910128e09e9e837125ce0db
Jeremy Myers <echelonical@gmail.com>
2022-12-04 17:58:58 -0600

    Update Documentation to list/retrieve functions

--- mcpressureapi/views/call_view.py
+++ mcpressureapi/views/call_view.py
@@ -9,13 +9,22 @@ class ServiceCallView(ViewSet):
     """Honey Rae API ServiceTicket view"""
      
     def list(self, request):
+        """Handle GET requests to get all customers
+
+        Returns:
+            Response -- JSON serialized list of customers
+        """
 
         service_call = ServiceCall.objects.all()
         serialized = ServiceCallSerializer(service_call, many=True)
         return Response(serialized.data, status=status.HTTP_200_OK)
 
     def retrieve(self, request, pk=None):
+        """Handle GET requests for single customer
 
+        Returns:
+            Response -- JSON serialized customer record
+        """
         service_call = ServiceCall.objects.get(pk=pk)
         serialized = ServiceCallSerializer(service_call, context={'request': request})
         return Response(serialized.data, status=status.HTTP_200_OK)
