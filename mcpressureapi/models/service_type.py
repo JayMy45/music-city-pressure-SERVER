36e98850e526783103ed6ce77a314caeaafd1947
Jeremy Myers <echelonical@gmail.com>
2022-12-06 09:29:59 -0600

    updated Appointment (removed location) and ServiceType (added location_id) models Updated fixtures.

--- mcpressureapi/models/service_type.py
+++ mcpressureapi/models/service_type.py
@@ -3,7 +3,6 @@ from django.db import models
 class ServiceType(models.Model):
     label = models.CharField(max_length=155)
     description = models.CharField(max_length=200)
-    location = models.ForeignKey("Location", on_delete=models.CASCADE)
     price = models.FloatField()
     equipment_id = models.ForeignKey('Equipment', blank=True, on_delete=models.CASCADE)
     name =  models.CharField(max_length=75)
