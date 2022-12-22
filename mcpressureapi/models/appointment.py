531d801564f5988121439c26ab38cf2614b1cc35
Jeremy Myers <echelonical@gmail.com>
2022-12-21 21:15:52 -0600

    update appointment.model to take images

--- mcpressureapi/models/appointment.py
+++ mcpressureapi/models/appointment.py
@@ -5,12 +5,13 @@ class Appointments(models.Model):
     customer = models.ForeignKey("Customer", on_delete=models.CASCADE, null=True, blank=True, related_name='confirm')
     service_type = models.ForeignKey("ServiceType", null=True, blank=True, on_delete=models.CASCADE)
     progress = models.ForeignKey("Progress", null=True, blank=True, on_delete=models.CASCADE)
+    request_details = models.CharField(max_length=200)
+    image = models.CharField(max_length=1000)
     request_date = models.DateField(null=True, blank=True, auto_now=False, auto_now_add=False)
-    scheduled = models.BooleanField(default=False)
     date_completed = models.DateField(null=True, blank=True, auto_now=False, auto_now_add=False)
+    scheduled = models.BooleanField(default=False)
     consultation = models.BooleanField(default=False)
     completed = models.BooleanField(default=False) 
-    request_details = models.CharField(max_length=200)
     confirm = models.BooleanField(default=False)
 
     @property
